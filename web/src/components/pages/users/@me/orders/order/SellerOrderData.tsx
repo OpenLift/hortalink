052fad2b10c321adc26cb5e9bb54c8d684a68a47
Victoreisdavid <65618679+Victoreisdavid@users.noreply.github.com>
2025-01-29 18:20:22 -0300

    Fix: Change button text and style to cancellation instead of update, due to backend behavior

--- web/src/components/pages/users/@me/orders/order/SellerOrderData.tsx
+++ web/src/components/pages/users/@me/orders/order/SellerOrderData.tsx
@@ -1,4 +1,7 @@
 import type { SellerOrder, SellerOrderProduct } from "@interfaces/Orders";
+import { OrderStatusText } from "@components/SellerOrder";
+import { useState } from "react";
+import APIWrapper, { RequestAPIFrom } from "@HortalinkAPIWrapper";
 
 const UNITS = {
     0: "kg",
@@ -12,10 +15,24 @@ const UNITS = {
 }
 
 export default function SellerOrderData(props: { product: SellerOrderProduct, fullOrder: SellerOrder }) {
+    const API = new APIWrapper(RequestAPIFrom.Client)
+
+    const [modalOpen, setModalOpen] = useState(false)
     let total = 0
 
     for(const product of props.fullOrder.products) {
         total += product.amount * product.price
+    }
+
+    async function confirmDelete() {
+        API.deleteOrder(props.product.order_id).then(() => {
+            window.location.reload()
+        }).catch(e => {
+            console.error(e)
+            alert("Não foi possível cancelar, acesse o console para ver o erro.")
+        })
+
+        setModalOpen(false)
     }
 
     return (
@@ -25,7 +42,7 @@ export default function SellerOrderData(props: { product: SellerOrderProduct, fu
                 #{props.product.order_id}
             </div>
             <h2>Dados</h2>
-            <p><span className="label_text">Status do pedido:</span> {props.product.status}</p>
+            <p><span className="label_text">Status do pedido:</span> {OrderStatusText[props.product.status] || "Em processamento"}</p>
             <p><span className="label_text">Retirada:</span> </p>
             <p className="label_text">Cliente:</p>
             <div className="client_card">
@@ -68,7 +85,31 @@ export default function SellerOrderData(props: { product: SellerOrderProduct, fu
             <h2>Resumo</h2>
             <p><span className="label_text">Qt Produtos:</span> {props.fullOrder.products.length}</p>
             <p><span className="label_text">Total:</span> <span className="price">R$ {total}</span></p>
-            <button className="update_button">Atualizar Status do pedido</button>
+            { props.product.status == 2 && <button className="update_button" onClick={() => setModalOpen(true)}>Cancelar pedido</button> }
+            {
+                modalOpen &&
+                <section className="modal_container">
+                    <section className="order_modal">
+                        <div className="modal_content">
+                            <section className="modal_header" onClick={() => setModalOpen(false)}>
+                                <h2>Cancelar pedido</h2>
+                                <img
+                                    src="/assets/X.svg"
+                                    width={23}
+                                    height={23}
+                                    alt="Imagem de um X. Clique para fechar a seção de filtros."
+                                    className="close"
+                                />
+                            </section>
+                            <p style={{ margin: "1rem auto", textAlign: "center" }}>Tem certeza que deseja cancelar o pedido?</p>
+                            <div className="order_buttons">
+                            <button className="order_button order_cancel" onClick={() => setModalOpen(false)}>Não</button>
+                            <button className="order_button order_confirm" onClick={confirmDelete}>Sim</button>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            }
         </main>
     )
 }
